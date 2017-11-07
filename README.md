228fc841b2627750dab6f2f6ada17e8926ba6324
Matt Trentini <matt.trentini@gmail.com>
2017-11-07 23:34:13 +1100

    First commit...doesn't quite work yet!

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# ssd1675a
-A Micropython driver for the SSD1675A
+# Micropython SSD1675A driver
+
+The [SSD1675](http://www.solomon-systech.com/en/product/advanced-display/Dot-Matrix_Bistable_Display/SSD1675/) 
+is a commonly used controller for E-Paper displays manufactured by [Solomon Systech](http://www.solomon-systech.com/). 
+
+This driver is initially trying to support 
+[Mike Rankin's ESP32 E-Paper board](https://twitter.com/mikerankin/status/916268694489096192) (thanks for the prototype Mike!)
+under [Micropython](https://micropython.org/). However the intent is to provide a useful Micropython driver for
+_any_ SSD1675a-powered display.
+
+## Other libraries
+
+[Drewler's Arduino driver](https://github.com/drewler/arduino-SSD1675A) was _very_ helpful,
+especially for initialization.
+
+[Radomir Dopieralski's drivers](https://bitbucket.org/thesheep/micropython-ili9341), particularly for the
+[SSD1606](https://bitbucket.org/thesheep/micropython-ili9341/src/1fd322f33fb68194e9a848d9fe5c74789aea15b8/ssd1606.py?at=default&fileviewer=file-view-default), were also useful:
+
+[Peter Hinch's epaper library](https://github.com/peterhinch/micropython-epaper) was another useful reference.
+
+## Example
+
+```python
+import ssd1675a
+from machine import Pin, SPI
+eink = ssd1675a.SSD1675A(spi=SPI(baudrate=100000, mosi=Pin(23, Pin.OUT), sck=Pin(18, Pin.OUT), miso=Pin(19, Pin.IN)),
+                         cs=Pin(4, Pin.OUT),
+                         dc=Pin(16, Pin.OUT),
+                         busy=Pin(17, Pin.IN))
+eink.reset()
+eink.rect(20, 20, 100, 100, 1)
+eink.show()
+```
---
+++ ssd1675a.py
@@ -0,0 +1,117 @@
+''' A Micropython driver for the SSD1675a EPaper controller.
+
+Initially trying to support Mike Rankin's ESP32 + EPaper board (thanks for the prototype Mike!)
+but the intent is to ultimately work for any SSD1675a-powered display.
+
+'''
+
+import framebuf
+import time
+
+class SSD1675A:
+    def __init__(self, spi, cs, dc, busy, width=296, height=128):
+        self.width = width
+        self.height = height
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        fb = framebuf.FrameBuffer(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+        self.framebuf = fb
+        # Provide methods for accessing FrameBuffer graphics primitives. This is a
+        # workround because inheritance from a native class is currently unsupported.
+        # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+        # TODO Need to extend to have two framebuffers - red and black.
+        # Will then need to _define_ these functions to allow a colour parameter and modify
+        # the appropriate framebuffer
+        self.fill = fb.fill
+        self.pixel = fb.pixel
+        self.hline = fb.hline
+        self.vline = fb.vline
+        self.line = fb.line
+        self.rect = fb.rect
+        self.fill_rect = fb.fill_rect
+        self.text = fb.text
+        self.scroll = fb.scroll
+        self.blit = fb.blit
+
+        # Set up the GPIO Pins
+        #self.rate = 10 * 1024 * 1024
+        dc.init(dc.OUT, value=0)
+        #res.init(res.OUT, value=0)
+        cs.init(cs.OUT, value=1)
+        busy.init(busy.IN)
+        self.spi = spi
+        self.dc = dc
+        #self.res = res
+        self.cs = cs
+        self.busy = busy
+
+        self._init_ssd1675a()
+
+    def write_cmd(self, cmd):
+        #self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        #self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(buf)
+        self.cs(1)
+
+    def write(self, cmd, data=None):
+        self.write_cmd(cmd)
+        if data:
+            self.write_data(data)
+
+    def _wait_busy(self):
+        while self.busy.value():
+            time.sleep_ms(10)
+
+    def _init_ssd1675a(self):
+        self.write(0x74, b'\x54') # Set Analog Block Control
+        self.write(0x7E, b'\x3B') # Set Digital Block Control (Note! There was an error in spi_demo.c, cmd was 75)
+        self.write(0x01, b'\x27\x01\x00') # Set MUX as 296
+        self.write(0x3A, b'\x35') # Set 130Hz (0x25=100Hz, 0x07=150Hz) (Note! 130Hz isn't listed in datasheet...)
+        self.write(0x3B, b'\x04') # Set 130Hz (0x06=100Hz)
+        self.write(0x3C, b'\x33') # Border Waveform Control: Level Setting for VBD=VSH2, GS Transition for VBD=LUT3
+        self.write(0x11, b'\x03') # Data Entry Mode (increment x and y and update in x direction)
+        self.write(0x44, b'\x00\x0F') # set RAM x address start/end, in page 36. Start at 0x00, end at 0x0F(15+1)*8->128
+        self.write(0x45, b'\x00\x00\x27\x01') # set RAM y address start/end, in page 37. Start at 0x127, end at 0x00.
+
+        self.write(0x04, b'\x41\xA8\x32') # set VSH1/VSH2/VSL values: 15/5/-15 respectively
+        self.write(0x2C, b'\x68') # VCOM = -2.6V
+        self._write_lut()
+
+    def _write_lut(self):
+        ''' Not really sure what these values mean. See 6.7 in the datasheet. Values taken
+        from drewler's spi_demo.c code.'''
+        self.write(0x32, b'\x22\x11\x10\x00\x10\x00\x00\x11\x88\x80\x80\x80\x00\x00'
+                         b'\x6A\x9B\x9B\x9B\x9B\x00\x00\x6A\x9B\x9B\x9B\x9B\x00\x00'
+                         b'\x00\x00\x00\x00\x00\x00\x00\x04\x18\x04\x16\x01\x0A\x0A'
+                         b'\x0A\x0A\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04'
+                         b'\x04\x08\x3C\x07\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+    def set_xy_window(self, xstart=0, xend=15, ystart=0, yend=295):
+        self.write(0x44, bytearray([xstart, xend])) # Set RAM x address start/end
+        self.write(0x45, bytearray([ystart, ystart >> 8, yend, yend >> 8])) # Set RAM y address start/end
+
+    def set_xy_counter(self, x, y):
+        self.write(0x4E, bytearray([x])) # Set RAM x address count
+        self.write(0x4F, bytearray([y, y>>8])) # Set RAM y address count
+
+    def show(self):
+        self.set_xy_window(0, 0x0F, 0, 0x127)
+        self.set_xy_counter(0, 0)
+        self.write(0x24, self.buffer) # Write data
+        self.write(0x22, bytearray([0xC7]))
+        self.write(0x20) # Update display
+        self._wait_busy()
+
+    def reset(self):
+        self.write(0x12) # SWRESET
+        self._wait_busy()
